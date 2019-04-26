b014dcd8ad4cb8a2374f00faf09a00a5de5e6ed1
chenshuai <1183942503@qq.com>
2019-04-26 19:08:57 +0800

    JSON配置文件

---
+++ JSON/BasicConfig.js
@@ -0,0 +1,54 @@
+const BasicConfig = {
+  Notice: [
+    {
+      title: "Hello！大家吼！这里是新的SAOMD人物图鉴首页喔~!",
+      href:"",
+      p:"",
+      time: "2019-04-19"
+    },
+    {
+      title: "Q&A",
+      href:"",
+      p:"关于本站",
+      time: ""
+    },
+    {
+      title: "游戏公告 / 下载&更新游戏",
+      href:"https://www.saomda.com/game",
+      p:"游戏公告-www.saomda.com",
+      time: ""
+    },
+    {
+      title: "玩家交流讨论的社区-<span style='color:#14B9C8;'>TapTap</span>",
+      href:"https://www.taptap.com/app/36771/topic",
+      p:"TapTap-刀剑神域:记忆重组(国际服)",
+      time: ""
+    },
+    {
+      title: "问题反馈（站长群）",
+      href:"https://jq.qq.com/?_wv=1027&k=5jMJSSF",
+      p:"点击加群：720339543",
+      time: ""
+    }
+  ],
+  newRoles: [
+    {
+      src: "./images/newRoles/017.png"
+    },
+    {
+      src: "./images/newRoles/016.png"
+    },
+    {
+      src: "./images/newRoles/015.png"
+    },
+    {
+      src: "./images/newRoles/014.png"
+    },
+    {
+      src: "./images/newRoles/012.png"
+    },
+    {
+      src: "./images/newRoles/011.png"
+    }
+  ]
+}
