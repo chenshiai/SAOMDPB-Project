e253424c8f40c3c8240ca05a6d8efb0fb3896297
chenshuai <1183942503@qq.com>
2019-05-04 14:50:29 +0800

    setCookie存储时间单位由天变为分钟

--- js/MPB.js
+++ js/MPB.js
@@ -346,7 +346,7 @@ const MPB = {
   //加一个上下出现的菜单组件
   "setCookie": function (cname, cvalue, exdays) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    d.setTime(d.getTime() + (exdays * 60 * 1000));
     var expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
   },
