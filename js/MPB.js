17902f1dd211f5890e2b21701544b411167fc7f6
chenshuai <1183942503@qq.com>
2019-05-23 23:46:29 +0800

    修改点格式

--- js/MPB.js
+++ js/MPB.js
@@ -540,30 +540,3 @@ const Scroll = {
     window.addEventListener('scroll', trigger);
   }
 }
-
-const a = [
-  {
-    id:1
-  },
-  {
-    id:4
-  },
-  {
-    id:2
-  },
-  {
-    id:3
-  },
-  {
-    id:5
-  },
-  {
-    id:7
-  },
-  {
-    id:6
-  },
-  {
-    id:8
-  }
-]
