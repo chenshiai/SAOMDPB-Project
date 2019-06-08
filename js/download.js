a18f704fc2809b18bfd7a8936319eff3de67f729
chenshuai <1183942503@qq.com>
2019-06-08 14:18:03 +0800

    图片下载区完成

--- js/download.js
+++ js/download.js
@@ -5,12 +5,116 @@ function downloadPageInit() {
     okFunc: () => { popup.hiddenPopup() },
     oneKey: true
   })
+  // 大图展示区
+  var roleImg = new Vue({
+    el: '#role-img',
+    data() {
+      return {
+        OSSURL: 'http://saoimages.oss-cn-shenzhen.aliyuncs.com/roles/allrole/allrole/character',
+        isShow: false,
+        bgYes: false,
+        roleinfo: {
+          cutin: '',
+          rare: '',
+          up: ''
+        },
+        roleImgList: [
+          {
+            url: '',
+            status: ''
+          }
+        ],
+        select: 0
+      }
+    },
+    computed: {
+
+    },
+    watch: {
+      roleinfo: function () {
+        this.roleImgList = [];
+        this.setRoleImgList();
+      },
+      select: function (newNum, oldNum) {
+        this.$set(this.roleImgList, oldNum, { url: this.roleImgList[oldNum].url, status: false });
+        this.$set(this.roleImgList, newNum, { url: this.roleImgList[newNum].url, status: true });
+      }
+    },
+    methods: {
+      close: function () {
+        this.isShow = false;
+      },
+      // 图片加载失败 移除该元素
+      loadError: function (index) {
+        this.roleImgList.splice(index, 1);
+        this.list.splice(index, 1);
+      },
+      bgselect: function () {
+        popup.showPopup({
+          text: '下载带背景立绘该功能尚未完善</br>想要带背景的高清立绘可以在站长群获得'
+        })
+        // this.bgYes = !this.bgYes;
+      },
+      // 选择第几张图
+      selectImg: function (index) {
+        this.select = index;
+      },
+      // 下载图片
+      download: function () {
+        if (this.bgYes) {
+          // 下载带背景的图未实现
+          popup.showPopup({
+            text: '下载带背景立绘该功能尚未完善</br>想要带背景的高清立绘可以在站长群获得'
+          })
+          this.isShow = false;
+        } else {
+          downloadIamge(this.thisRoleUrl(), this.roleinfo.cutin);
+          this.isShow = false;
+        }
+      },
+      thisRoleUrl: function () {
+        if (this.select > 0) {
+          return this.getImg(this.roleinfo.cutin + `_${this.select}`, false);
+        } else {
+          return this.getImg(this.roleinfo.cutin, false);
+        }
+      },
+      // 根据角色星级判断是否多张
+      listnum: function () {
+        if (this.roleinfo.rare == 3) return true;
+        if (this.roleinfo.rare == 2 && this.roleinfo.up == 1) return true;
+        return false;
+      },
+      setRoleImgList: function () {
+        let roleinfo = this.roleinfo;
+        if (this.listnum()) {
+          this.roleImgList.push({ url: this.getImg(roleinfo.cutin) });
+          for (let i = 1; i < 5; i++) {
+            this.roleImgList.push({ url: this.getImg(roleinfo.cutin + `_${i}`) });
+          }
+        } else {
+          this.roleImgList.push({ url: this.getImg(roleinfo.cutin) });
+        }
+        this.select = 0;
+        this.$set(this.roleImgList, 0, { url: this.roleImgList[0].url, status: true });
+      },
+      getImg: function (cutin, smoll = true) {
+        if (!cutin) return 'null.png';
+        if (smoll) {
+          return this.OSSURL + cutin.slice(cutin.indexOf('_')) + '.png?x-oss-process=style/tosmoll';
+        } else {
+          return this.OSSURL + cutin.slice(cutin.indexOf('_')) + '.png';
+        }
+      },
+    }
+  })
+
   // 角色列表Vue模块
   var roleList = new Vue({
     el: '#role-list',
     data() {
       return {
-        OSSURL: 'https://saoimages.oss-cn-shenzhen.aliyuncs.com/roles/allrole/allrole/character',
+        OSSURL: 'http://saoimages.oss-cn-shenzhen.aliyuncs.com/roles/allrole/allrole/character',
         roleList: [{ // 默认值防止异步报错
           cutin: '',
           title: '',
@@ -29,10 +133,13 @@ function downloadPageInit() {
     },
     methods: {
       // 根据角色编号拼接图片地址 character_102001_1
-      getImg: function (cutin) {
+      getImg: function (cutin, smoll = true) {
         if (!cutin) return 'null.png';
-        let url = this.OSSURL + cutin.slice(cutin.indexOf('_')) + '.png?x-oss-process=style/tosmoll';
-        return url;
+        if (smoll) {
+          return this.OSSURL + cutin.slice(cutin.indexOf('_')) + '.png?x-oss-process=style/tosmoll';
+        } else {
+          return this.OSSURL + cutin.slice(cutin.indexOf('_')) + '.png';
+        }
       },
       // 拼接本地的图片地址
       localImg: function (cutin) {
@@ -70,7 +177,8 @@ function downloadPageInit() {
       // 点击li打开全部立绘
       toBigImg: function (index) {
         if (this.roleList[index].status) {
-          console.log(this.roleList[index]);
+          roleImg.roleinfo = this.roleList[index];
+          roleImg.isShow = true;
         } else {
           popup.showPopup({
             title: 'Message',
@@ -94,6 +202,49 @@ function downloadPageInit() {
       })
     }
   });
+
+  // 下载逻辑
+  function downloadIamge(imgsrc, name) {
+    //下载图片地址和图片名
+    console.log(imgsrc);
+    var image = new Image();
+    // 解决跨域 Canvas 污染问题
+    image.setAttribute('crossOrigin', 'anonymous');
+    image.onload = function () {
+      var canvas = document.createElement('canvas');
+      canvas.width = image.width;
+      canvas.height = image.height;
+      var context = canvas.getContext('2d');
+      context.drawImage(image, 0, 0, image.width, image.height);
+      var _dataURL = canvas.toDataURL('image/png'); //得到图片的base64编码数据
+
+      var blob_ = dataURLtoBlob(_dataURL); // 用到Blob是因为图片文件过大时，在一些浏览器上会下载失败，而Blob就不会
+
+      var url = {
+        name: name || "图片", // 图片名称不需要加.png后缀名
+        src: blob_
+      };
+
+      if (window.navigator.msSaveOrOpenBlob) {   // if browser is IE
+        navigator.msSaveBlob(url.src, url.name);//filename文件名包括扩展名，下载路径为浏览器默认路径
+      } else {
+        var link = document.createElement("a");
+        link.setAttribute("href", window.URL.createObjectURL(url.src));
+        link.setAttribute("download", url.name + '.png');
+        link.click();
+      }
+    };
+    image.src = imgsrc;
+
+    function dataURLtoBlob(dataurl) {
+      var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+      }
+      return new Blob([u8arr], { type: mime });
+    }
+  }
   // 公告点击展开按钮
   var nwit = 1;
   $('#NoticeOpen').click(function () {
